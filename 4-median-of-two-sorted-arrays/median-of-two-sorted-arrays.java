833dccd863b202a3724b441db766f1cc99ad494d
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-04-03 23:51:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (43.65%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,50 @@
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+//     }
+// }
+
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            // If partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+            // If partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // We have partitioned array at correct place
+                if ((x + y) % 2 == 0) {
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                // Move towards the left in nums1
+                high = partitionX - 1;
+            } else {
+                // Move towards the right in nums1
+                low = partitionX + 1;
+            }
+        }
+
+        // If input arrays are not sorted or not valid for median calculation
+        throw new IllegalArgumentException("Input arrays are not sorted or not valid for median calculation.");
+    }
+}
