1b87f569bf707ea57a8a7eaa35d8238adf7f5904
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-04-15 02:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (30.60%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,24 +1,25 @@
 class Solution {
+    public void swap(int[] arr , int mid ,int low ){
+    int temp = arr[mid];
+    arr[mid] = arr[low];
+    arr[low] = temp;
+    }
     public void sortColors(int[] arr) {
       //Dutch Algorithm
         int n = arr.length;
         int low = 0, mid = 0,  high = n - 1;
-        while (mid <= high)
-        {
-                if (arr[mid] == 0) {
-                    int temp = arr[mid];
-                    arr[mid] = arr[low];
-                    arr[low] = temp;
 
-                    low++;
-                    mid++;
-                } else if (arr[mid]== 1) mid++;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, mid, low);
+                low++;
+                 mid++;
+                }
 
-                else  if(arr[mid] == 2)
-                {
-                    int temp = arr[mid];
-                    arr[mid] = arr[high];
-                    arr[high] = temp;
+            else if (arr[mid]== 1) mid++;
+
+            else  if(arr[mid] == 2){ // arr[mid] == 2
+                    swap(arr,mid,high);
                     high--;
                 }
 
