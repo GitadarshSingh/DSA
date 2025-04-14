9161ece8839794fb2bf436eaf8cc6260d2bbd1c9
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-04-15 02:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (82.23%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,25 +1,29 @@
 class Solution {
     public void sortColors(int[] arr) {
-        //method 1. with two pass solution
-         int n = arr.length;
-        int noOfZeroes = 0 , noOfOnes = 0;
-        for (int i = 0; i < n; i++)
+      //Dutch Algorithm
+        int n = arr.length;
+        int low = 0, mid = 0,  high = n - 1;
+        while (mid <= high)
         {
-            if (arr[i] == 0) noOfZeroes++;
-            if(arr[i] == 1 ) noOfOnes++;
+                if (arr[mid] == 0) {
+                    int temp = arr[mid];
+                    arr[mid] = arr[low];
+                    arr[low] = temp;
+
+                    low++;
+                    mid++;
+                } else if (arr[mid]== 1) mid++;
+
+                else  if(arr[mid] == 2)
+                {
+                    int temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                }
 
         }
-        for (int i = 0; i < n; i++)
-        {
-            if (i < noOfZeroes) arr[i] = 0;
-            else if (i<noOfZeroes+noOfOnes) {
-                arr[i] = 1;
-            }
-            else arr[i] = 2;
-        }
-        for (int ele : arr)
-        {
-            System.out.print(ele+" ");
-        }
+  
+    
     }
 }
