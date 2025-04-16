ed9c8b7b05c3532fdaad2c0b84d19b070ae9c3f2
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-04-16 16:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (8.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public void merge(int[] a , int m, int[] b, int n) {
     int[] c = new int[m+n];
-
     int i = 0 , j = 0 , k = 0;
 
     while(i<m && j<n)
@@ -19,13 +18,6 @@ class Solution {
         }
     
     }
-    // if(j == n){// now take elemnts form b only
-    //        while(i<m)
-    //        {
-    //            c[k] = a[i];
-    //            i++; k++;
-    //        }
-    //    }
 
      while(i<m)
          {
@@ -41,10 +33,17 @@ class Solution {
              j++;
             k++;
          }
-         for (int p = 0; p < m + n; p++) {
-            a[p] = c[p];
-        
-    }
-    }
+
+         // We are storing all the element of 'c' array in 'a' array because function is void we can't retun 'c',
+      int x = 0;
+while (x < m + n) {
+    a[x] = c[x];
+    x++;
+}
+
+    
+
+
+}
     
 }
