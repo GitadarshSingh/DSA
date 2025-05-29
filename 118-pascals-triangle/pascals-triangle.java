227dd2cf775417e8a74f02be2cf488f63fd866ef
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-05-29 22:39:16 +0530

    Time: 1 ms (84.77%) | Memory: 42.3 MB (25.64%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,32 +1,19 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-
-        // First base case
-        if (numRows <= 0) {
-            return triangle;
-        }
-
-        // First row is always [1]
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
-
-        // Start from the second row
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> prevRow = triangle.get(i - 1);
-            List<Integer> row = new ArrayList<>();
-
-            row.add(1); // first element is always 1
-
-            // Each element (except the first and last) is sum of the two above it
-            for (int j = 1; j < i; j++) {
-                row.add(prevRow.get(j - 1) + prevRow.get(j));
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                l.add(1);
             }
-
-            row.add(1); // last element is always 1
-            triangle.add(row);
+            ans.add(l);
         }
-
-        return triangle;
+        for(int i=2;i<n;i++){
+            for(int j=1;j<=i-1;j++){
+               // ans.get(i).set(j) = ans.get(i-1).get(i)+ans.get(i-1).get(j-1);
+               ans.get(i).set(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+            }
+        }
+        return ans;
     }
 }
