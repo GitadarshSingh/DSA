c9b814e29acd36553ee2e6ac604cfecc5db320c7
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-06-10 23:06:18 +0530

    Time: 2 ms (25.18%) | Memory: 42.8 MB (32.39%) - LeetSync

--- 223-rectangle-area/rectangle-area.java
+++ 223-rectangle-area/rectangle-area.java
@@ -2,20 +2,20 @@ class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2,
                            int bx1, int by1, int bx2, int by2) {
 
-        // Area of Rectangle A
+        // Rectangle A
         int areaA = (ax2 - ax1) * (ay2 - ay1);
 
-        // Area of Rectangle B
+        //Rectangle B
         int areaB = (bx2 - bx1) * (by2 - by1);
 
-        
+        //Overlapping Rect
         int overWidth = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
         int overHeight = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
 
-        // Overlapping Area
+        
         int overArea = overWidth * overHeight;
 
-        // Total Area (Union)
+        // Total Area
         return areaA + areaB - overArea;
     }
 }
