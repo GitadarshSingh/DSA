3f5df1e9fc84fa4cdd8a8095e3a5063e8816df1a
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-06-27 10:57:26 +0530

    Time: 132 ms (5.03%) | Memory: 46.2 MB (40.74%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,36 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int start = 0;
-        int end = start + 1;
+    //   ArrayList<Integer> arr = new ArrayList();
+    //   int noz = 0;
+    //   for(int ele:nums)
+    //   {
+    //     if(ele!=0) arr.add(ele);
+    //     else noz++;
+    //   }
+    //   for(int i=0;i<noz;i++){
+    //     arr.add(0);
+    //   }
+    //   for(int i=0; i<nums.length;i++)
+    //   {
+    //     nums[i] = arr.get(i);
+    //   }
 
-        while(end < n){
-            if(nums[start]==0 && nums[end] != 0){
-               int temp = nums[start];
-               nums[start] = nums[end];
-               nums[end] = temp;
-                start++;
-            }else if(nums[start] == 0 && nums[end]==0){
-                end++;
-             }else {//if(nums[start] != 0)
-               start++;
-                if(end <= start) end = start + 1;
+    int n = nums.length;
+    int noz=0;
+    for(int ele:nums)
+    {
+        if(ele == 0) noz++;
+    }
+    for(int i=0;i<noz;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(nums[j] == 0){
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
             }
-    
         }
+    }
     }
 }
