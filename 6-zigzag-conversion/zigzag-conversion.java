15e028bcf97bb44d789bac5ea9efc3572e84448b
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-06-29 00:50:26 +0530

    Time: 3 ms (90.14%) | Memory: 44.9 MB (69.67%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -24,7 +24,7 @@ class Solution {
                 {
                     idx = idx+deltaUp;
                 }
-                else if(goingSouth == true) 
+                else if(goingSouth) 
                 {
                     idx = idx+deltaBelow;
                 }
