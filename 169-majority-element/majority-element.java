4d09985bcb7c6a8e3fb2a89132f86d2b8203d45d
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-07-04 02:02:12 +0530

    Time: 76 ms (7.66%) | Memory: 52.7 MB (69.55%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,10 +1,23 @@
 class Solution {
+    public void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int mid = nums[n/2];
+        // Arrays.sort(nums);
+        // int mid = nums[nums.length/2];
+        // return mid;
 
-        return mid;
+        for (int i = 1; i < nums.length ; i++) {
+            for (int j = i; j >= 1; j--) {
+           if(nums[j] < nums[j-1]){
+               swap(nums, j , j-1);
+           }else break;
+
+            }
+        }
+        return nums[nums.length/2];
         
     }
 }
