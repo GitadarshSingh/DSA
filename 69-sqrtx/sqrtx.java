6de54e644d3e8a64f9d0ca5d00f757a75a4622a9
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-07-06 20:33:01 +0530

    Time: 1 ms (86.52%) | Memory: 40.9 MB (69.35%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)(Math.sqrt(x)); // newton rapson method
+        long lo = 0 , hi = x;
+        while(lo <= hi)
+        {
+            long mid = lo + (hi - lo)/2;
+            if(mid*mid == x) return (int)mid;
+            else if(mid*mid > x) hi = mid - 1;
+            else lo = mid + 1;
+
+        }
+        return (int)hi;
     }
 }
