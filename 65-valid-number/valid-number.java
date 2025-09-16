25326e570137f74e70c412710489d5cdde485e5b
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-09-16 23:25:21 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (34.03%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isNumber(String s) {
+        if (s == null || s.isEmpty()) return false;
+        s = s.trim();
+        boolean seenDigit = false, seenDot = false, seenExp = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+            } 
+            else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+            } 
+            else if (c == '.') {
+                if (seenDot || seenExp) return false;
+                seenDot = true;
+            } 
+            else if (c == 'e' || c == 'E') {
+                if (seenExp || !seenDigit) return false;
+                seenExp = true;
+                seenDigit = false; 
+            } 
+            else {
+                return false;
+            }
+        }
+        return seenDigit;
+    }
+}
