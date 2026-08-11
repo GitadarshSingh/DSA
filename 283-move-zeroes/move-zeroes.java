eb046c644aa6ccee4c3547609747d892990bb2af
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2026-08-11 15:14:38 +0530

    Time: 2 ms (92.06%) | Memory: 47.9 MB (38.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,36 +1,19 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-    //   ArrayList<Integer> arr = new ArrayList();
-    //   int noz = 0;
-    //   for(int ele:nums)
-    //   {
-    //     if(ele!=0) arr.add(ele);
-    //     else noz++;
-    //   }
-    //   for(int i=0;i<noz;i++){
-    //     arr.add(0);
-    //   }
-    //   for(int i=0; i<nums.length;i++)
-    //   {
-    //     nums[i] = arr.get(i);
-    //   }
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
 
-    int n = nums.length;
-    int noz=0;
-    for(int ele:nums)
-    {
-        if(ele == 0) noz++;
-    }
-    for(int i=0;i<noz;i++)
-    {
-        for(int j=0;j<n-1-i;j++)
-        {
-            if(nums[j] == 0){
-                int temp = nums[j];
-                nums[j] = nums[j+1];
-                nums[j+1] = temp;
+        int i = 0; // "khali front jagah" ka pointer
+
+        for (int j = 0; j < n; j++) {
+            if (arr[j] != 0) {
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++; // agli khali jagah ready
             }
+            
         }
-    }
     }
 }
