ddfc440695db4baab427909b369a6182ef26ea7b
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2026-08-11 15:35:58 +0530

    Time: 2 ms (92.06%) | Memory: 48.1 MB (6.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,18 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int n = arr.length;
+        int n=arr.length;
 
-        int i = 0; // "khali front jagah" ka pointer
+       int i=0;
+       for(int j=0;j<n;j++)
+       {
+        if(arr[j]!=0)
+        {
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
 
-        for (int j = 0; j < n; j++) {
-            if (arr[j] != 0) {
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
-                i++; // agli khali jagah ready
-            }
-            
+            i++;
         }
+       }
     }
 }
